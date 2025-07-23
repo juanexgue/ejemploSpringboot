ebb4e47d42cfbc46cf79d887fdf41e8be3fe0dad
Juan Guevara M. <juanexgue@gmail.com>
2025-07-23 18:39:38 -0500

    Use RequestMapping

--- src/main/java/com/juanex/springboot_application/controllers/EjemploRestController.java
+++ src/main/java/com/juanex/springboot_application/controllers/EjemploRestController.java
@@ -5,16 +5,23 @@ import java.util.Map;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
+@RequestMapping("/api")
 public class EjemploRestController {
 
-@GetMapping("/info2")
+@RequestMapping(path="/info2", method=RequestMethod.GET)
     public Map<String, Object> info(){
     Map<String, Object> respuesta = new HashMap<>();
     respuesta.put("Titulo", "Servidor en linea");
+    respuesta.put("Servidor", "Juanex");
+    respuesta.put("Ip", "192.168.1.1");
     return respuesta;
     }
 
