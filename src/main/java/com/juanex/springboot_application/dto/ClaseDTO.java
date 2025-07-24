333ae7f4de3f80ce220813fcc0e0364a219ff986
Juan Guevara M. <juanexgue@gmail.com>
2025-07-23 20:22:05 -0500

    use clase DTO

---
+++ src/main/java/com/juanex/springboot_application/dto/ClaseDTO.java
@@ -0,0 +1,23 @@
+package com.juanex.springboot_application.dto;
+
+public class ClaseDTO {
+
+    private String titulo, usuario;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+}
