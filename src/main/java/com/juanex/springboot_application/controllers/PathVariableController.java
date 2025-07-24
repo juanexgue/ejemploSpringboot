8c7b63d53a9fe468f977757c2ca007bcd2cf985f
Juan Guevara M. <juanexgue@gmail.com>
2025-07-23 21:10:40 -0500

    use PostMapping

--- src/main/java/com/juanex/springboot_application/controllers/PathVariableController.java
+++ src/main/java/com/juanex/springboot_application/controllers/PathVariableController.java
@@ -2,9 +2,14 @@ package com.juanex.springboot_application.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.juanex.springboot_application.models.Empleados;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/variable")
@@ -15,6 +20,12 @@ public class PathVariableController {
         var parametro1 = new ParametroDTO();
         parametro1.setInformacion(mensaje);
         return parametro1;
+    }
+
+    @PostMapping("/solicitud")
+    public Empleados creaEmpleado(@RequestBody Empleados empleado1) {
+        return empleado1;
+
     }
 
 }
