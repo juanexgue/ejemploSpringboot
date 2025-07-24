a461d46216f88a097b1a6f5a7ef0f1145007b434
Juan Guevara M. <juanexgue@gmail.com>
2025-07-23 19:19:33 -0500

    Use Model

--- src/main/java/com/juanex/springboot_application/controllers/EjemploController.java
+++ src/main/java/com/juanex/springboot_application/controllers/EjemploController.java
@@ -4,14 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.juanex.springboot_application.models.Empleados;
+
 
 @Controller
 public class EjemploController {
 
 @GetMapping("/info")
     public String info(Model model){
+
+    Empleados empleado1 = new Empleados("Juan", "Perez",
+     "Micaela", "Developer", 30, 92343424,8);
+
     model.addAttribute("Titulo","Servidor en linea");
-    model.addAttribute("Servidor", "Juanexgue");
+    model.addAttribute("empleado1", empleado1);
         return "detalles_info";
     }
 
--- src/main/java/com/juanex/springboot_application/controllers/EjemploRestController.java
+++ src/main/java/com/juanex/springboot_application/controllers/EjemploRestController.java
@@ -3,12 +3,12 @@ package com.juanex.springboot_application.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.juanex.springboot_application.models.Empleados;
+
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -17,11 +17,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class EjemploRestController {
 
 @RequestMapping(path="/info2", method=RequestMethod.GET)
+
+
     public Map<String, Object> info(){
+
+    Empleados empleado1 = new Empleados("Juan", "Perez",
+     "Micaela", "Developer", 30, 92343424,8);
+
     Map<String, Object> respuesta = new HashMap<>();
-    respuesta.put("Titulo", "Servidor en linea");
-    respuesta.put("Servidor", "Juanex");
-    respuesta.put("Ip", "192.168.1.1");
+    respuesta.put("Empleado", empleado1);
+
     return respuesta;
     }
 
